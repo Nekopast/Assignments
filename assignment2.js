c18fe9a16898166e0e9e1050859cd419a6af6aeb
Aaron Payne <aaron_payne@hotmail.co.uk>
2023-06-02 11:10:57 +0100

    second commit

--- assignment2.js
+++ assignment2.js
@@ -5,3 +5,11 @@ let lunch = "2 corn beef sandwiches and a pack of crisps"
 let dinner = "burger and chips"
 
 console.log(`Today for breakfast I had ${breakfast}, for lunch I had ${lunch} and for dinner I had ${dinner}`)
+
+breakfast = "Kraves"
+
+lunch = "2 Burgers and chips"
+
+dinner = "Spaghetti Bolognese"
+
+console.log(`on this day I had ${breakfast}, for lunch ${lunch} and for dinner ${dinner}`)
